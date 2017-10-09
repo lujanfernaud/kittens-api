0c52923b37acb218cb5b352086ca3144a891a2d8
lujanfernaud <lujanfernaud@gmail.com>
2017-10-09 22:00:46 +0100

    Add seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,18 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Kitten.create!(name: "Pinky",
+               age: 2,
+               cuteness: "Good",
+               softness: "Good")
+
+Kitten.create!(name: "Matisse",
+               age: 4,
+               cuteness: "Good",
+               softness: "Good")
+
+Kitten.create!(name: "Acen",
+               age: 4,
+               cuteness: "Good",
+               softness: "Good")
